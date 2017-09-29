ffb1724195d17494c88855e6ec205a04574df456
allexramon <alexramonsg@gmail.com>
2017-09-29 16:41:48 -0300

    Comandos no GIT - Commit

--- index.php
+++ index.php
@@ -4,14 +4,24 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+//rota usuário comum
 $app->get('/', function() {
   	
 	$page= new Page();
+	$page->setTpl("index");	
+	
+});
+
+//rota usuário adm
+$app->get('/admin', function() {
+  	
+	$page= new PageAdmin();
 	$page->setTpl("index");	
 	
 });
